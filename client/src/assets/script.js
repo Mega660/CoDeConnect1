2bb351f2687e1504cf7d086487d278c80f61f480
Kojo-Syntax <albert.gbeve@stu.ucc.edu.gh>
2024-06-15 18:15:07 +0000

    first javascript

---
+++ client/src/assets/script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const BASE_URL = "http://127.0.0.1:8000";
+    
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const formData = {
+            email: document.querySelector('input[type = "email"]').value,
+            username: document.querySelector('input[type="text"]').value,
+            password: document.querySelector('input[type = "password"').value,
+        };
+
+        fetch (`${BASE_URL}/register/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+    const signupButton = document.querySelector('button');
+    signupButton.addEventListener('click', handleFormSubmit);
+});

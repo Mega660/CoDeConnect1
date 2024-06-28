1c1c70955d87e2ddf28bcb41c028c5b85b0d3572
dansarpong <dansarpong.me@gmail.com>
2024-06-28 01:45:19 +0000

    Add login and register links

--- client/src/assets/login.js
+++ client/src/assets/login.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const handleFormSubmit = (event) => {
         event.preventDefault(); 
-
         const formData = {
             email: document.querySelector('input[type="email"]').value,
             password: document.querySelector('input[type="password"]').value,
@@ -17,15 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             body: JSON.stringify(formData),
         })
-        .then (response => response.json())
-        .then (data => {
-            console.log (data);
-        })
-
-        .catch(error=> {
-            console.error('Error:', error);
-        });
+        .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          console.log(data);
+          // switch to dashboard page after a few seconds
+          setTimeout(() => {
+              window.location.href = "dashboard.html";
+          }, 3000);
+      })
+      .catch(error => {
+          console.log(error);
+          // remove hidden class from error message
+          const errorMessage = document.getElementById("errorMessage");
+          errorMessage.classList.remove("hidden");
+      });
     };
-    const loginButton = document.getElementById('signinButton');
+    const loginButton = document.getElementById('loginButton');
     loginButton.addEventListener('click', handleFormSubmit);
 })
--- client/src/assets/register.js
+++ client/src/assets/register.js
@@ -16,13 +16,25 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             body: JSON.stringify(formData),
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+        .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          console.log(data);
+          // redirect to login page after a few seconds
+          setTimeout(() => {
+              window.location.href = "login.html";
+          }, 3000);
+      })
+      .catch(error => {
+          console.log(error);
+          // remove hidden class from error message
+          const errorMessage = document.getElementById("errorMessage");
+          errorMessage.classList.remove("hidden");
+      });
     }
     const signupButton = document.getElementById("signupButton");
     signupButton.addEventListener('click', handleFormSubmit);
